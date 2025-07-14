c06579fe30a811ca6e8769c3f0b8750720836b3d
priti2110 <sumanpriti35@gmail.com>
2025-07-14 19:48:43 +0530

    Update AppRunner.java

--- AppRunner.java
+++ AppRunner.java
@@ -1,2 +1,14 @@
-public class AppRunner { public static void main(String[] args) { System.out.println("Java active"); } }
-// update 1.1
+﻿// this is java script
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, App.add(2, 3));
+    }
+}
